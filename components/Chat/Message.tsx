113ab41e53e9aedceb2d68c651f04f9dd015287d
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-02 02:56:04 +0900

    I split summary note component.

--- components/Chat/Message.tsx
+++ components/Chat/Message.tsx
@@ -4,6 +4,7 @@ import { useToolCallProvider } from "@/providers/ToolCallProvider";
 import ToolFollowUp from "../Tools/ToolFollowUp";
 import Icon from "../Icon";
 import { useChatProvider } from "@/providers/ChatProvider";
+import ReportSummaryNote from "./ReportSummaryNote";
 
 const Message = ({ message, index }: { message: AIMessage; index: number }) => {
   const { context, loading } = useToolCallProvider();
@@ -20,30 +21,7 @@ const Message = ({ message, index }: { message: AIMessage; index: number }) => {
         {context && <ToolContent />}
         <ToolFollowUp message={message} />
         {reportEnabled && index === 0 && !pending && !loading && (
-          <>
-            <button
-              type="button"
-              className="text-purple-dark mt-6"
-            >{`[Download Full Report PDF]`}</button>
-            <p className="py-4 text-[20px]">Next Steps</p>
-            <ul className="text-[14px] space-y-2 ml-5">
-              <li className="list-disc">
-                <span className="font-bold">
-                  Explore Partnership Opportunities:
-                </span>{" "}
-                Select a suggested brand to generate a tailored pitch deck.
-              </li>
-              <li className="list-disc">
-                <span className="font-bold">Refine Content Ideas:</span> Get
-                recommendations for TikTok content tailored to this segment..
-              </li>
-              <li className="list-disc">
-                <span className="font-bold">Monitor & Update:</span> Enable
-                continuous tracking for this segment to uncover new trends and
-                engagement opportunities. Ongoing Tracking Enabled ✅
-              </li>
-            </ul>
-          </>
+          <ReportSummaryNote />
         )}
       </div>
     </div>
