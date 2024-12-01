bf17013f4f26270e39cad47ad6ef185e1cbbe95c
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-02 01:48:29 +0900

    I fix style

--- lib/tools/toolSystemMessage.tsx
+++ lib/tools/toolSystemMessage.tsx
@@ -32,7 +32,7 @@ const toolSystemMessage = (context: any, question: any, toolName: string) => {
     ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}
     NOTE: 
     - For **lists of data**, use **commas** as separators instead of <li> tags.
-    - **Sub-title** of sections should be 14px & font-bold.
+    - **Sub-titles** for sections such as **Size**, **Demo**, **Trends**, **Top Brands** should be 14px & font-bold & <span />.
     - **Title** of response should be 18px & font-bold and bottom padding 12px.`;
 
   return "";
