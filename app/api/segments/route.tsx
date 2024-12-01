16ed4886bc24d020190654cb92082632ae6d67be
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-12-01 11:45:20 +0900

    I update instruction to include icon

--- app/api/segments/route.tsx
+++ app/api/segments/route.tsx
@@ -18,7 +18,7 @@ export async function POST(req: NextRequest) {
         },
         {
           role: "system",
-          content: `${instructions.get_fan_segments} \n Response should be in JSON format. {"data": [{ "string": number }, { "string": number }]}.`,
+          content: `${instructions.get_fan_segments} \n Response should be in JSON format. {"data": [{ "string": { number, "string" } }, { "string": number, "string" }]}.`,
         },
       ],
     });
