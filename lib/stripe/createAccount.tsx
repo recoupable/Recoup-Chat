0a0313aa4e4588638d1fb98b63683c2543263820
tech322 <tech.eng.me@gmail.com>
2024-12-07 07:35:41 +0900

    add: toastify package

--- lib/stripe/createAccount.tsx
+++ lib/stripe/createAccount.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const createAccount = async (accountData: any) => {
   try {
-    const response = await fetch('https://api.stripe.com/v1/accounts', {
-      method: 'POST',
+    const response = await fetch("https://api.stripe.com/v1/accounts", {
+      method: "POST",
       headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRIPE_SK}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: new URLSearchParams(accountData).toString(),
     });
