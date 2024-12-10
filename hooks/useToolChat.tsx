be1e385c6b5e4379ce402af2f7125ff9c7d838cf
Va~~~dym <tech.eng.me@gmail.com>
2024-12-10 05:40:59 -0800

    fix: unsave issue

--- hooks/useToolChat.tsx
+++ hooks/useToolChat.tsx
@@ -104,7 +104,6 @@ const useToolChat = (question?: string, toolName?: any) => {
     };
     if (
       !loading &&
-      chatMessages?.length === 2 &&
       toolMessages?.length === 2 &&
       tiktokRawReportContent &&
       tiktokNextSteps &&
