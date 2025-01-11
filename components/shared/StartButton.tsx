b5f6e1730d0f1f76687ead01a8e0dac911afbbe6
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-11 13:32:50 -0300

    add Button from Shadcn     - install button     - use button in <StartButton />

--- components/shared/StartButton.tsx
+++ components/shared/StartButton.tsx
@@ -1,14 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 interface StartButtonProps {
   onClick?: () => void;
 }
 
 export function StartButton({ onClick }: StartButtonProps) {
   return (
-    <button
+    <Button
       onClick={onClick}
-      className="p-1.5 rounded-md border-[1px] border-grey"
+      size="lg"
+      className="px-12 py-6 text-lg font-semibold hover:scale-105 transition-transform"
     >
       Start
-    </button>
+    </Button>
   );
 }
