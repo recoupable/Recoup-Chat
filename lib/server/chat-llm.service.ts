bf412b798ad3d34cc85a2fa9f0e3e086a5cb63b7
dev <tech.eng.me@gmail.com>
2025-02-04 15:09:15 -0800

    update ai model

--- lib/server/chat-llm.service.ts
+++ lib/server/chat-llm.service.ts
@@ -1,12 +1,10 @@
 import "server-only";
 
-import { openai } from "@ai-sdk/openai";
-import { CoreTool, LanguageModelV1, streamText } from "ai";
-import { encodeChat } from "gpt-tokenizer";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 import { z } from "zod";
+import OpenAI from "openai";
 
 import { createChatMessagesService } from "./chat-messages.service";
-import { AI_MODEL } from "../consts";
 
 export const ChatMessagesSchema = z.object({
   messages: z.array(
@@ -23,84 +21,45 @@ export const StreamResponseSchema = ChatMessagesSchema.extend({
   context: z.string(),
 });
 
-/**
- * @name createChatLLMService
- * @description Create a new instance of the ChatLLMService.
- */
 export function createChatLLMService() {
   return new ChatLLMService();
 }
 
-/**
- * @name ChatLLMService
- * @description Chat service that uses the LLM model to generate responses.
- */
 class ChatLLMService {
   constructor() {}
-
-  /**
-   * @name streamResponse
-   * @description Stream a response to the user and store the messages in the database.
-   */
   async streamResponse({
     messages,
     email,
     artistId,
     context,
   }: z.infer<typeof StreamResponseSchema>) {
-    // use a normal service instance using the current user RLS
     const chatMessagesService = createChatMessagesService();
-
-    // get the last message
     const lastMessage = messages[messages.length - 1];
 
     if (!lastMessage) {
       throw new Error("No messages provided");
     }
 
-    // // make sure the user has enough credits
-    // await this.assertEnoughCredits(accountId);
-
-    // retrieve the chat settings
     const chatContext =
       messages.length > 2
         ? context || messages[messages.length - 2].content
         : context;
-    const settings = await chatMessagesService.getChatSettings(
+    await chatMessagesService.getChatSettings(
       lastMessage.content,
       email,
       artistId,
       chatContext,
     );
-    const systemMessage = settings.systemMessage;
-    const maxTokens = settings.maxTokens;
-    const tools = settings.tools;
-    // we need to limit the history length so not to exceed the max tokens of the model
-    // let's assume for simplicity that all models have a max tokens of 128000
-    // so we need to make sure that the history doesn't exceed output length + system message length
-    const maxModelTokens = 228000;
 
-    const maxHistoryLength = maxModelTokens - systemMessage.length - maxTokens;
-    let decodedHistory = encodeChat(messages, AI_MODEL);
+    const openai = new OpenAI();
 
-    if (decodedHistory.length > maxHistoryLength) {
-      while (decodedHistory.length > maxHistoryLength) {
-        messages.shift();
-        decodedHistory = encodeChat(messages, AI_MODEL);
-      }
-    }
-
-    // we use the openai model to generate a response
-    const result = await streamText({
-      model: openai(settings.model) as LanguageModelV1,
-      system: settings.systemMessage,
-      maxTokens: settings.maxTokens,
-      temperature: 0.7,
-      messages,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      tools: tools as Record<string, CoreTool<any, any>> | undefined,
+    const response = await openai.chat.completions.create({
+      model: "o3-mini",
+      stream: true,
+      messages: messages,
     });
 
-    return result.toDataStreamResponse();
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
   }
 }
