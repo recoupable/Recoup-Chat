1ea5b39f72db6328a52e155f731cd13dc48f9325
Sweets Sweetman <sweetmantech@gmail.com>
2025-02-21 13:43:02 -0300

    SegmentButton component.

--- components/Funnels/Segments.tsx
+++ components/Funnels/Segments.tsx
@@ -1,5 +1,6 @@
 import useGenerateSegmentReport from "@/hooks/useGenerateSegmentReport";
 import { type Segment } from "@/lib/supabase/getArtistSegments";
+import SegmentButton from "./SegmentButton";
 
 interface SegmentsProps {
   segments: Segment[];
@@ -13,20 +14,11 @@ const Segments = ({ segments }: SegmentsProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 pt-4 gap-3">
       {sortedSegments.map((segment) => (
-        <button
+        <SegmentButton
           key={segment.id}
-          onClick={() => handleGenerateReport(segment.id, segment.name)}
-          className="bg-black rounded-[10px] pl-5 pr-4 h-16 z-20 flex items-center gap-2 justify-between
-            transition-all text-[15px] font-medium text-white hover:bg-black active:bg-white/80"
-        >
-          <div className="flex flex-col items-start">
-            <p className="text-sm text-start">{segment.name}</p>
-            <p className="text-xs text-grey-primary">{segment.size} fans</p>
-          </div>
-          <div className="text-xs text-grey-primary whitespace-nowrap">
-            Generate Report
-          </div>
-        </button>
+          segment={segment}
+          onGenerateReport={handleGenerateReport}
+        />
       ))}
     </div>
   );
