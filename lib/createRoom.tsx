2fc26b6cc08832fa9b1dc2083a6821b3e14818e2
Sweets Sweetman <sweetmantech@gmail.com>
2025-02-25 12:16:57 -0300

    I rollback lib/createRoom.tsx:

--- lib/createRoom.tsx
+++ lib/createRoom.tsx
@@ -1,8 +1,11 @@
-const createRoom = async (account_id: string, title: string) => {
+import getAiTitle from "./getAiTitle";
+
+const createRoom = async (account_id: string, content: string) => {
   try {
+    const title = await getAiTitle(content);
     const topic = title.replaceAll(`\"`, "");
     const response = await fetch(
-      `/api/room/create?account_id=${account_id}&topic=${encodeURIComponent(topic)}`
+      `/api/room/create?account_id=${account_id}&topic=${encodeURIComponent(topic)}`,
     );
     const data = await response.json();
     return data.new_room;
