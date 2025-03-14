41b57a73aa1f0f113244d1f323e8824e03006535
Sidney Swift <158200036+sidneyswift@users.noreply.github.com>
2025-03-14 16:25:44 -0400

    MYC-1266: Chat Page Updates When Switching Artists

--- components/Artists/Artist.tsx
+++ components/Artists/Artist.tsx
@@ -2,6 +2,7 @@ import { useArtistProvider } from "@/providers/ArtistProvider";
 import { ArtistRecord } from "@/types/Artist";
 import ImageWithFallback from "../ImageWithFallback";
 import DropDown from "./DropDown";
+import { usePathname, useRouter } from "next/navigation";
 
 const Artist = ({
   artist,
@@ -10,10 +11,24 @@ const Artist = ({
   artist: ArtistRecord;
   isVisibleDropDown: boolean;
 }) => {
-  const { setSelectedArtist, setMenuVisibleArtistId } = useArtistProvider();
+  const { setSelectedArtist, setMenuVisibleArtistId, selectedArtist } = useArtistProvider();
+  const pathname = usePathname();
+  const { push } = useRouter();
 
   const handleClick = () => {
     setMenuVisibleArtistId(null);
+    
+    // Only proceed with redirection if we're actually switching artists
+    if (selectedArtist?.account_id !== artist.account_id) {
+      // UUID pattern: 8-4-4-4-12 hexadecimal characters
+      const uuidPattern = /^\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+      const isChatPage = uuidPattern.test(pathname);
+      
+      if (isChatPage || pathname.includes("/funnels")) {
+        push("/");
+      }
+    }
+    
     setSelectedArtist(artist);
   };
 
