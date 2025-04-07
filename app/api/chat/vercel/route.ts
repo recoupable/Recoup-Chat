32debc59c2ee80e912267ead47c4e087da476bd0
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-07 13:40:46 -0400

    I add maxSteps with tool call streaming

--- app/api/chat/vercel/route.ts
+++ app/api/chat/vercel/route.ts
@@ -38,6 +38,8 @@ export async function POST(request: NextRequest) {
       }),
     ],
     messages,
+    maxSteps: 11,
+    toolCallStreaming: true,
   });
 
   return stream.toDataStreamResponse({
