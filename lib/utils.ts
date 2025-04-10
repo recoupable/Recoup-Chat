cf8c0851627f1357a25cc6a0e91dae20ca500a71
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-10 17:00:39 -0400

    utils rollback changes.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Combines Tailwind CSS classes with the clsx utility
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
