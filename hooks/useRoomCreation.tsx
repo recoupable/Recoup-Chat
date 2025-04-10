16e7da328ab4a0a71a6624c941e8a019ee0f8bcb
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-10 16:51:19 -0400

    refactor chatId => id.

--- hooks/useRoomCreation.tsx
+++ hooks/useRoomCreation.tsx
@@ -19,12 +19,12 @@ export function useRoomCreation({
   const [isCreatingRoom, setIsCreatingRoom] = useState(false);
   const { addConversation } = useConversationsProvider();
 
-  const createNewRoom = async (content: string, chatId?: string) => {
+  const createNewRoom = async (content: string, id?: string) => {
     if (roomId || isCreatingRoom || !userId) return;
 
     try {
       setIsCreatingRoom(true);
-      const room = await createRoom(userId, content, artistId, chatId);
+      const room = await createRoom(userId, content, artistId, id);
 
       if (room) {
         // Update internal state first
