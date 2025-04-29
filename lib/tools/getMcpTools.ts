2e2e99e8b9d44f983da7fdf790644e3253c6fbe4
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-28 22:13:43 -0300

    P1: tool - get_social_posts     actual: missing tool get_social_posts to call API /api/social/posts     required: add tool get_social_posts to call/api/social/posts matching docs     docs: https://recoup-docs.vercel.app/social/posts

--- lib/tools/getMcpTools.ts
+++ lib/tools/getMcpTools.ts
@@ -4,6 +4,7 @@ import contactTeam from "./contactTeam";
 import getArtistComments from "./getArtistComments";
 import getArtistSegments from "./getArtistSegments";
 import getArtistSocials from "./getArtistSocials";
+import getSocialPosts from "./getSocialPosts";
 import { getPerplexityTools } from "./getPerplexityTools";
 
 export async function getMcpTools() {
@@ -23,6 +24,7 @@ export async function getMcpTools() {
     get_artist_segments: getArtistSegments,
     get_artist_socials: getArtistSocials,
     get_segment_fans: getSegmentFans,
+    get_social_posts: getSocialPosts,
     ...toolSetMantleWebSearch,
     ...perplexityTools,
   };
