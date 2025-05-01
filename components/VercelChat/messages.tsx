0d1a3b48a008bc139617e86aea08ee5cbc0aa97a
veerbal1 <veerbal.singh369@gmail.com>
2025-05-01 14:01:54 +0530

    Improved chat messages auto scrolling based intersection observer

--- components/VercelChat/messages.tsx
+++ components/VercelChat/messages.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { SpinnerIcon } from "./icons";
 import { UIMessage } from "ai";
 import { UseChatHelpers } from "@ai-sdk/react";
 import Message from "./message";
 import Markdown from "../Chat/ChatMarkdown-v2/markdown";
+import { useInView } from "react-intersection-observer";
 
 interface TextMessagePartProps {
   text: string;
@@ -31,17 +32,21 @@ export function Messages({
   children,
 }: MessagesProps) {
   const messagesRef = useRef<HTMLDivElement>(null);
-  const messagesLength = useMemo(() => messages.length, [messages]);
+
+  const { ref, inView } = useInView({
+    root: messagesRef.current,
+    threshold: 0,
+  });
 
   useEffect(() => {
     const scrollContainer = messagesRef.current;
-    if (scrollContainer) {
+    if (scrollContainer && inView) {
       // Use requestAnimationFrame to scroll after the next browser paint
       requestAnimationFrame(() => {
         scrollContainer.scrollTop = scrollContainer.scrollHeight;
       });
     }
-  }, [messages, status]); // Add status to dependencies for streaming updates
+  }, [messages, status, inView]); // Add status to dependencies for streaming updates
 
   return (
     <div
@@ -66,6 +71,9 @@ export function Messages({
           </div>
         </div>
       )}
+      <div ref={ref} className="h-4 bg-transparent select-none opacity-0 z-10 w-full flex items-center justify-center text-white font-bold" >
+        {inView ? 'Yes' : 'No'}
+      </div>
     </div>
   );
 }
