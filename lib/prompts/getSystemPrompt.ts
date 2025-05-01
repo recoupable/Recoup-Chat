c77b83955ce35697629273c05f10f3581d751992
veerbal1 <veerbal.singh369@gmail.com>
2025-05-01 12:21:01 +0530

    Added mermaid system instruction for LLM api call

--- lib/prompts/getSystemPrompt.ts
+++ lib/prompts/getSystemPrompt.ts
@@ -1,4 +1,4 @@
-import { SYSTEM_PROMPT } from "@/lib/consts";
+import { MERMAID_INSTRUCTIONS_PROMPT, SYSTEM_PROMPT } from "@/lib/consts";
 import getKnowledgeBaseContext from "@/lib/agent/getKnowledgeBaseContext";
 import getArtistIdForRoom from "../supabase/getArtistIdForRoom";
 
@@ -28,7 +28,11 @@ export async function getSystemPrompt({
 ${knowledge}
 -----END KNOWLEDGE BASE-----`;
   }
-
+  systemPrompt = `${systemPrompt}
+  
+-----MERMAID INSTRUCTIONS-----
+${MERMAID_INSTRUCTIONS_PROMPT}
+-----END MERMAID INSTRUCTIONS-----`;
   return systemPrompt;
 }
 
