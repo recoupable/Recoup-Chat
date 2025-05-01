a75f1c79ca49eba4c88634c971fa857d8aa6c8f0
veerbal1 <veerbal.singh369@gmail.com>
2025-05-01 19:04:29 +0530

    Updated tailwind classes to improve mobile responsiveness

--- components/VercelChat/chat.tsx
+++ components/VercelChat/chat.tsx
@@ -58,7 +58,7 @@ export function Chat({ id, reportId }: ChatProps) {
   return (
     <div
       className={cn(
-        "px-4 md:px-0 pb-4 pt-8 flex flex-col h-full items-center w-full max-w-3xl",
+        "px-4 md:px-0 pb-4 mb:pt-8 flex flex-col h-full items-center w-full max-w-3xl",
         {
           "justify-between": messages.length > 0,
           "justify-center gap-4": messages.length === 0,
