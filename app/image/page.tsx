11664187c365785c0a91d7f54ea55b48dcf11c9f
Sweets Sweetman <sweetmantech@gmail.com>
2025-04-30 22:24:18 -0300

    attempting to center content.

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -2,18 +2,20 @@ import { ImageGenerator } from "./image-generator";
 
 const ImagePage = () => {
   return (
-    <div className="w-full max-w-3xl space-y-6">
-      <div className="text-center space-y-2">
-        <h1 className="text-3xl font-bold tracking-tight">
-          Base Image Generator
-        </h1>
-        <p className="text-muted-foreground">
-          Enter a prompt and generate an image using AI and auto-published
-          onchain with base
-        </p>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
+      <div className="w-full max-w-3xl space-y-6">
+        <div className="text-center space-y-2">
+          <h1 className="text-3xl font-bold tracking-tight">
+            Base Image Generator
+          </h1>
+          <p className="text-muted-foreground">
+            Enter a prompt and generate an image using AI and auto-published
+            onchain with base
+          </p>
+        </div>
+        <ImageGenerator />
       </div>
-      <ImageGenerator />
-    </div>
+    </main>
   );
 };
 
