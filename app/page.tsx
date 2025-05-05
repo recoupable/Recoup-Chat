2690c7ca72899b540c364c0d3b1a02d5f3878942
Sweets Sweetman <sweetmantech@gmail.com>
2025-05-05 18:50:40 -0300

    search params updated to nextJS expected type.

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import { getMessages } from "@/lib/messages/getMessages";
 export const dynamic = "force-dynamic";
 
 interface ChatPageProps {
-  searchParams?: { q?: string };
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export default async function Home({ searchParams }: ChatPageProps) {
   const id = generateUUID();
-  const initialMessage = searchParams?.q || "";
+  const initialMessage = (await searchParams)?.q as string;
   const initialMessages = getMessages(initialMessage);
 
   return <HomePage id={id} initialMessages={initialMessages} />;
