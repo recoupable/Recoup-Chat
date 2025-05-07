a10554c9db360c32006778c8479789fa52a901b5
Sweets Sweetman <sweetmantech@gmail.com>
2025-05-07 18:20:02 -0300

    new lib getSpotifyImage

--- components/VercelChat/tools/GetSpotifySearchToolResult.tsx
+++ components/VercelChat/tools/GetSpotifySearchToolResult.tsx
@@ -1,21 +1,6 @@
 import React from "react";
 import { SpotifySearchResponse } from "@/types/spotify";
-
-function getImage(item: unknown): string | undefined {
-  if (typeof item === "object" && item !== null) {
-    const obj = item as {
-      images?: { url: string }[];
-      album?: { images?: { url: string }[] };
-    };
-    if (obj.images && obj.images.length > 0) {
-      return obj.images[0].url;
-    }
-    if (obj.album && obj.album.images && obj.album.images.length > 0) {
-      return obj.album.images[0].url;
-    }
-  }
-  return undefined;
-}
+import { getSpotifyImage } from "@/lib/spotify/getSpotifyImage";
 
 const typeLabels: Record<string, string> = {
   artists: "Artists",
@@ -32,54 +17,44 @@ const GetSpotifySearchToolResult: React.FC<{
 }> = ({ result }) => {
   return (
     <div>
-      {Object.entries(result)
-        .filter(
-          ([key, value]) =>
-            key !== "success" &&
-            value &&
-            typeof value === "object" &&
-            "items" in (value as object) &&
-            Array.isArray((value as { items?: unknown[] }).items) &&
-            ((value as { items?: unknown[] }).items?.length ?? 0) > 0
-        )
-        .map(([key, value]) => {
-          const section = value as { items: unknown[] };
-          return (
-            <div key={key} className="mb-6">
-              <div className="font-semibold text-lg mb-2">
-                {typeLabels[key] || key}
-              </div>
-              <div className="flex flex-nowrap overflow-x-auto pb-1 -mx-2">
-                {section.items.map((item) => {
-                  const obj = item as { id?: string; name?: string };
-                  return (
-                    <div
-                      key={obj.id || Math.random()}
-                      className="w-[140px] border border-gray-200 rounded-lg p-2 m-2 text-center bg-white flex-shrink-0 flex flex-col items-center justify-start"
-                    >
-                      {getImage(item) && (
-                        <div className="w-full flex justify-center">
-                          {/* eslint-disable-next-line @next/next/no-img-element */}
-                          <img
-                            src={getImage(item)}
-                            alt={obj.name || ""}
-                            className="w-[100px] h-[100px] object-cover rounded-md mb-1 block"
-                          />
-                        </div>
-                      )}
-                      <div
-                        className="font-medium text-[15px] max-w-[120px] truncate mx-auto"
-                        title={obj.name}
-                      >
-                        {obj.name}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
+      {Object.entries(result).map(([key, value]) => {
+        const section = value as { items: unknown[] };
+        return (
+          <div key={key} className="mb-6">
+            <div className="font-semibold text-lg mb-2">
+              {typeLabels[key] || key}
             </div>
-          );
-        })}
+            <div className="flex flex-nowrap overflow-x-auto pb-1 -mx-2">
+              {section.items.map((item) => {
+                const obj = item as { id?: string; name?: string };
+                return (
+                  <div
+                    key={obj.id || Math.random()}
+                    className="w-[140px] border border-gray-200 rounded-lg p-2 m-2 text-center bg-white flex-shrink-0 flex flex-col items-center justify-start"
+                  >
+                    {getSpotifyImage(item) && (
+                      <div className="w-full flex justify-center">
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img
+                          src={getSpotifyImage(item)}
+                          alt={obj.name || ""}
+                          className="w-[100px] h-[100px] object-cover rounded-md mb-1 block"
+                        />
+                      </div>
+                    )}
+                    <div
+                      className="font-medium text-[15px] max-w-[120px] truncate mx-auto"
+                      title={obj.name}
+                    >
+                      {obj.name}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
