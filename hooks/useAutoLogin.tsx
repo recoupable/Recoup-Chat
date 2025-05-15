b4f9fd0de678454a5de30cc47dea99791677cca5
Sweets Sweetman <sweetmantech@gmail.com>
2025-05-15 13:08:23 -0300

    only try autoLogin if not in a miniapp.

--- hooks/useAutoLogin.tsx
+++ hooks/useAutoLogin.tsx
@@ -3,18 +3,22 @@
 import { useEffect, useRef } from "react";
 import { usePrivy } from "@privy-io/react-auth";
 import { useUserProvider } from "@/providers/UserProvder";
+import { useMiniKit } from "@coinbase/onchainkit/minikit";
 
 export function useAutoLogin() {
   const { login } = usePrivy();
   const { email } = useUserProvider();
+  const { context } = useMiniKit();
+
   const hasTriedLogin = useRef(false);
 
   useEffect(() => {
-    if (!email && !hasTriedLogin.current) {
+    const shouldTryLogin = !email && !hasTriedLogin.current && !context;
+    if (shouldTryLogin) {
       hasTriedLogin.current = true;
       login();
     }
-  }, [email, login]);
+  }, [email, login, context]);
 }
 
-export default useAutoLogin; 
+export default useAutoLogin;
