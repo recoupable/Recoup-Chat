09337d0ed449fd05e06f4da771b58a9900e9cfd1
Sweets Sweetman <sweetmantech@gmail.com>
2025-05-27 14:51:52 -0300

    new lib saveApifyInstagramPosts

--- lib/apify/handleApifyWebhook.ts
+++ lib/apify/handleApifyWebhook.ts
@@ -1,8 +1,7 @@
 import getDataset from "@/lib/apify/getDataset";
 import { ApifyInstagramPost } from "@/types/Apify";
-import insertPosts from "@/lib/supabase/posts/insertPosts";
-import getPosts from "@/lib/supabase/posts/getPosts";
-import { TablesInsert, Tables } from "@/types/database.types";
+import saveApifyInstagramPosts from "@/lib/apify/saveApifyInstagramPosts";
+import { Tables } from "@/types/database.types";
 import { apifyPayloadSchema } from "@/app/api/apify/route";
 import { z } from "zod";
 
@@ -21,15 +20,12 @@ export default async function handleApifyWebhook(
     try {
       const dataset = await getDataset(datasetId);
       if (Array.isArray(dataset) && dataset[0]?.latestPosts) {
-        const posts = dataset[0].latestPosts.map(
-          (post: ApifyInstagramPost) => ({
-            post_url: post.url,
-            updated_at: post.timestamp,
-          })
-        ) as TablesInsert<"posts">[];
-        const postUrls = posts.map((post) => post.post_url);
-        saveResult = await insertPosts(posts);
-        supabasePosts = await getPosts(postUrls);
+        const { saveResult: sr, supabasePosts: sp } =
+          await saveApifyInstagramPosts(
+            dataset[0].latestPosts as ApifyInstagramPost[]
+          );
+        saveResult = sr;
+        supabasePosts = sp;
       }
     } catch (e) {
       console.error("Failed to handle Apify webhook:", e);
