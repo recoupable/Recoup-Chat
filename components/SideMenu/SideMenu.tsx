0b23ed5f89d40eeb50552aea1ba8a57d2565d59b
Sweets Sweetman <sweetmantech@gmail.com>
2025-05-29 14:24:47 -0300

    only show subscribe cta if !miniApp

--- components/SideMenu/SideMenu.tsx
+++ components/SideMenu/SideMenu.tsx
@@ -10,6 +10,7 @@ import { v4 as uuidV4 } from "uuid";
 import { useArtistProvider } from "@/providers/ArtistProvider";
 import { PointerIcon } from "lucide-react";
 import { Button } from "../ui/button";
+import { useMiniAppProvider } from "@/providers/MiniAppProvider";
 
 const SideMenu = ({
   isVisible,
@@ -23,6 +24,7 @@ const SideMenu = ({
   const { push } = useRouter();
   const { address, isPrepared } = useUserProvider();
   const { selectedArtist, sorted, toggleCreation } = useArtistProvider();
+  const { isMiniApp } = useMiniAppProvider();
   const hasArtists = sorted.length > 0;
   const isArtistSelected = !!selectedArtist;
 
@@ -96,7 +98,7 @@ const SideMenu = ({
       </div>
       {address && <RecentChats toggleModal={toggleModal} />}
       <div className="grow flex flex-col gap-1 md:gap-3 justify-end">
-        <UnlockPro />
+        {!isMiniApp && <UnlockPro />}
         <UserInfo toggleMenuExpanded={toggleModal} />
       </div>
     </SideModal>
