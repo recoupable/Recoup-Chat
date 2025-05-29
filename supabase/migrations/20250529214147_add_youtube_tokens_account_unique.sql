19736d186f4f78c0dc24775ed6353a9b6fded142
veerbal1 <veerbalsingh1@gmail.com>
2025-05-29 22:38:02 +0530

    Test commit: file

---
+++ supabase/migrations/20250529214147_add_youtube_tokens_account_unique.sql
@@ -0,0 +1,5 @@
+-- This migration was already applied remotely
+-- Content moved to 20250529222923_youtube_tokens_consolidated.sql
+-- Keeping this file to maintain migration history consistency
+
+SELECT 1; -- No-op 
