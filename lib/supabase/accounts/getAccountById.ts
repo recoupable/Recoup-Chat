eebb19d6af030eb227f084af533d056806cd3dac
sweetman.eth <sweetmantech@gmail.com>
2025-05-29 13:19:50 -0300

    Sweetmantech/myc 2079 apiaccountupdate supabase libs (#670)
    * handleApifyWebhook - normalize profile_url lib for getSocialByProfileUrl
    
    * remove excessive dev logging
    
    * remove excessive dev logging.
    
    * remove supabase prefixes to variable names.
    
    * reduce duplicated if checks.
    
    * getAccountArtistIdsByArtistId - update to more generic getAccountArtistIds so I can pass array of account_ids to get all account_artist_ids for those accounts
    
    * handleApifyWebhook - firstResult.profilePicUrl - update to arweave url (upload original image to arweave and use the arweave url)
    
    * /api/apify - remove cache
    
    * generate_text - send email
    
    * generate_text - send email
    
    * remove dev logging.
    
    * supabase - account_wallets table - unique wallet
    
    * /api/account/update - supabase libs
    
    * spread the response for expected response in useUser

--- lib/supabase/accounts/getAccountById.ts
+++ lib/supabase/accounts/getAccountById.ts
@@ -1,12 +1,20 @@
 import supabase from "@/lib/supabase/serverClient";
 import type { Tables } from "@/types/database.types";
 
+export type AccountWithInfoAndEmail = Tables<"accounts"> & {
+  account_emails: Array<Pick<Tables<"account_emails">, "email">>;
+  account_info: Array<Tables<"account_info">>;
+  account_wallets: Array<Tables<"account_wallets">>;
+};
+
 const getAccountById = async (
   id: string
-): Promise<Tables<"accounts"> | null> => {
+): Promise<AccountWithInfoAndEmail | null> => {
   const { data } = await supabase
     .from("accounts")
-    .select("*")
+    .select(
+      "*, account_emails(email), account_info(*), account_wallets(wallet)"
+    )
     .eq("id", id)
     .single();
   return data || null;
