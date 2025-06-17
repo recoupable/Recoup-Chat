d34d80835a7e0729cad0e9ef212bb8ebf11955dd
veerbal1 <veerbalsingh1@gmail.com>
2025-06-18 00:02:32 +0530

    Minor fix

--- utils/getYoutubeChannelNameFromURL.ts
+++ utils/getYoutubeChannelNameFromURL.ts
@@ -3,9 +3,9 @@ export const getYoutubeChannelNameFromURL = (url: string) => {
   const urlParts = url.split("/");
   youtubeChannelName = urlParts[urlParts.length - 1] || "";
   if (youtubeChannelName.startsWith("c/")) {
-    youtubeChannelName = "@" + youtubeChannelName.substring(2);
+    youtubeChannelName = youtubeChannelName.substring(2);
   } else if (youtubeChannelName.startsWith("@")) {
     youtubeChannelName = youtubeChannelName.substring(1);
   }
-  return youtubeChannelName;
+  return `@${youtubeChannelName}`;
 };
