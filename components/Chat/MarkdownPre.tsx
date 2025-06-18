070d90415797e75e1ef9984222770aa6e0966dbd
veerbal1 <veerbalsingh1@gmail.com>
2025-06-18 22:22:05 +0530

    Code cleanup

--- components/Chat/MarkdownPre.tsx
+++ components/Chat/MarkdownPre.tsx
@@ -9,7 +9,7 @@ const MarkdownPre: React.FC<MarkdownPreProps & React.HTMLAttributes<HTMLPreEleme
   ...props 
 }) => {
   return (
-    <pre className="!rounded-xl !max-w-xs md:!max-w-2xl !mx-auto" {...props}>
+    <pre className="!max-w-xs md:!max-w-2xl !mx-auto" {...props}>
       {children}
     </pre>
   );
