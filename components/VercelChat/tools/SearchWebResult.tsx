b442adea97e6173d2fdeeb87ad774dec9a483745
veerbal1 <veerbalsingh1@gmail.com>
2025-06-26 20:04:02 +0530

    Fixed double expand button issue for small web search result

--- components/VercelChat/tools/SearchWebResult.tsx
+++ components/VercelChat/tools/SearchWebResult.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import ChatMarkdown from "@/components/Chat/ChatMarkdown";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Maximize2, Minimize2 } from "lucide-react";
+import useContainerHeight from "@/hooks/useContainerHeight";
 
 export interface SearchWebResultType {
   content: {
@@ -15,6 +16,12 @@ export interface SearchWebResultType {
 
 const SearchWebResult= ({ result }: { result: SearchWebResultType }) => {
   const [expanded, setExpanded] = useState(false);
+  const [showTopExpandButton, setShowTopExpandButton] = useState(false);
+  const { containerRef, containerHeight } = useContainerHeight();
+
+  useEffect(() => {
+    setShowTopExpandButton(containerHeight ? containerHeight > 400 : false);
+  }, [expanded, containerHeight]);
 
   if (result.isError) {
     return (
@@ -45,14 +52,14 @@ const SearchWebResult= ({ result }: { result: SearchWebResultType }) => {
       <CardContent className="pt-4 p-4">
         <div className="flex justify-between items-center">
           <h3 className="text-lg font-medium mb-2">Web Search Results</h3>
-          {expanded ? expandButton() : null}
+          {showTopExpandButton ? (expanded ? expandButton() : null) : null}
         </div>
         <div className={"prose prose-sm dark:prose-invert max-w-none relative"}>
           {result.content.map((item, index) => (
-            <div key={index} className={cn("mb-4 [&_p]:text-xs [&_li]:text-xs [&_td]:text-xs [&_th]:text-xs [&_h2]:text-sm overflow-hidden")}
+            <div ref={containerRef} key={index} className={cn("mb-4 [&_p]:text-xs [&_li]:text-xs [&_td]:text-xs [&_th]:text-xs [&_h2]:text-sm overflow-hidden")}
               style={{
                 transition: "max-height 0.3s cubic-bezier(0.4, 0, 0.2, 1)",
-                maxHeight: expanded ? 2000 : 200,
+                maxHeight: expanded ? 5000 : 200,
               }}
             >
               {item.type === "text" && <ChatMarkdown>{item.text}</ChatMarkdown>}
